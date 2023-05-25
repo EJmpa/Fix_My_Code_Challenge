1fc65ff34ae8879771df927d5fd02b40f0b055c4
Chukwu Chukwuemeka Jude <111859279+EJmpa@users.noreply.github.com>
2023-05-25 14:43:24 +0100

    Update 0-fizzbuzz.py

--- 0x00-challenge/0-fizzbuzz.py
+++ 0x00-challenge/0-fizzbuzz.py
@@ -7,7 +7,7 @@ import sys
 def fizzbuzz(n):
     """
     FizzBuzz function prints numbers from 1 to n separated by a space.
-
+    
     - For multiples of three print "Fizz" instead of the number and for
       multiples of five print "Buzz".
     - For numbers which are multiples of both three and five print "FizzBuzz".
@@ -28,7 +28,7 @@ def fizzbuzz(n):
     print(" ".join(tmp_result))
 
 
-if __name__ != '__main__':
+if __name__ == '__main__':
     if len(sys.argv) <= 1:
         print("Missing number")
         print("Usage: ./0-fizzbuzz.py <number>")
